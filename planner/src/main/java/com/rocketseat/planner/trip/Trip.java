b78c4c38d00f69a0ea69faafe646347252095fa8
Samuel Carneiro <samuelcarneiro.al@gmail.com>
2024-07-09 02:11:00 -0300

    Aula 1 finalizada com sucesso!

---
+++ planner/src/main/java/com/rocketseat/planner/trip/Trip.java
@@ -0,0 +1,53 @@
+package com.rocketseat.planner.trip;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.format.DateTimeFormatter;
+import java.util.UUID;
+
+import static java.time.format.DateTimeFormatter.*;
+
+@Entity
+@Table(name = "trips")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Trip {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String destination;
+
+    @Column(name = "starts_at", nullable = false)
+    private LocalDateTime startsAt;
+
+    @Column(name = "ends_at", nullable = false)
+    private LocalDateTime endsAt;
+
+    @Column(name = "is_confirmed", nullable = false)
+    private Boolean isConfirmed;
+
+    @Column(name = "owner_name", nullable = false)
+    private String ownerName;
+
+    @Column(name = "owner_email", nullable = false)
+    private String ownerEmail;
+
+    public Trip(TripRequestPayload data) {
+        this.destination = data.destination();
+        this.isConfirmed = false;
+        this.ownerEmail = data.owner_email();
+        this.ownerName = data.owner_name();
+        this.startsAt = LocalDateTime.parse(data.starts_at(), ISO_DATE_TIME);
+        this.endsAt = LocalDateTime.parse(data.ends_at(), ISO_DATE_TIME);
+    }
+}
